7aaf12a9816808792fc5eb40853dd7bf60f5b882
ViniciusTanaca <vinicius_tanaca@outlook.com>
2025-04-22 20:25:34 -0300

    Implementando cálculo automático do total de gastos

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ form.addEventListener('submit', function (e) {
   const descricao = document.getElementById('descricao').value;
   const valor = parseFloat(document.getElementById('valor').value);
   const categoria = document.getElementById('categoria').value;
+
+  if (descricao && !isNaN(valor) && categoria) {
+    gastos.push({ descricao, valor, categoria });
+    atualizarLista();
+    form.reset();
+  }
+});
+function atualizarLista() {
+    lista.innerHTML = '';
+    let total = 0;
+  
+    gastos.forEach((gasto, index) => {
+      total += gasto.valor;
+  
+      const li = document.createElement('li');
+      li.innerHTML = `
+        <span>${gasto.descricao} - 
+          R$ <span class="${gasto.valor > 100 ? 'vermelho' : ''}">
+          ${gasto.valor.toFixed(2)}</span> 
+          [${gasto.categoria}]
+        </span>
+       
